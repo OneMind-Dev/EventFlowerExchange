c1534fd79cd5a93f7e840f1d8b37215232b5a8d5
PhuThang <thangtpse151058@fpt.edu.vn>
2024-10-01 22:24:26 +0700

    small change

--- EventFlowerExchange-Frontend/event_flower-resell/src/pages/user/userProfile.jsx
+++ EventFlowerExchange-Frontend/event_flower-resell/src/pages/user/userProfile.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import Header from '../../components/header/header'
-import Footer from '../../components/footer/footer'
-import '../../components/user/userProfile.css'
+import React from 'react';
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+import '../../components/user/userProfile.css';
 import { Form, Input, Button } from "antd";
 import { FaCircleUser } from "react-icons/fa6";
-import FormItem from "antd/es/form/FormItem";
-
 
 function UserProfile() {
     return (
@@ -15,49 +13,70 @@ function UserProfile() {
                 <div className='user_container'>
                     <div className='user_infor'>
                         <FaCircleUser className='user_icon' />
-                        <h2 className='username'>
-                            Nibba
-                        </h2>
+                        <h2 className='username'>Nibba</h2>
                     </div>
                     <div className='user_interact'>
-                        <h3 className='privateInfor'>
-                            Hồ sơ cá nhân
-                        </h3>
-                        <p>
-                            Thay đổi mật khẩu
-                        </p>
-                        <p>
-                            Đơn hàng
-                        </p>
+                        <h3 className='privateInfor'>Hồ sơ cá nhân</h3>
+                        <p>Thay đổi mật khẩu</p>
+                        <p>Đơn hàng</p>
                     </div>
                 </div>
                 <div className='infor_container'>
-                    <h1>
-                        Thông tin cá nhân
-                    </h1>
-                    <p>
-                        Tên tài khoản
-                        <FormItem
-                            className="input-box"
-                            name="username"
-                        >
-                            <Input className="input" type="text" placeholder="" />
-                        </FormItem>
-                    </p>
-                    <p>
-                        Họ và tên
-                    </p>
-                    <p>
-                        Số điện thoại
-                    </p>
-                    <p>
-                        Email
-                    </p>
-                    <p>
-                        Địa chỉ
-                    </p>
+                    <h1>Thông tin cá nhân</h1>
+                    <Form layout="vertical">
+                        <div className="form-item">
+                            <label htmlFor="username">Tên tài khoản</label>
+                            <Form.Item
+                                name="username"
+                                rules={[{ required: true, message: 'Vui lòng nhập tên tài khoản!' }]}
+                            >
+                                <Input placeholder="" />
+                            </Form.Item>
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="fullname">Họ và tên</label>
+                            <Form.Item
+                                name="fullname"
+                                rules={[{ required: true, message: 'Vui lòng nhập họ và tên!' }]}
+                            >
+                                <Input placeholder="" />
+                            </Form.Item>
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="phone">Số điện thoại</label>
+                            <Form.Item
+                                name="phone"
+                                rules={[{ required: true, message: 'Vui lòng nhập số điện thoại!' }]}
+                            >
+                                <Input placeholder="" />
+                            </Form.Item>
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="email">Email</label>
+                            <Form.Item
+                                name="email"
+                                rules={[{ required: true, message: 'Vui lòng nhập email!' }]}
+                            >
+                                <Input placeholder="" />
+                            </Form.Item>
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="address">Địa chỉ</label>
+                            <Form.Item
+                                name="address"
+                                rules={[{ required: true, message: 'Vui lòng nhập địa chỉ!' }]}
+                            >
+                                <Input className='address' placeholder="" />
+                            </Form.Item>
+                        </div>
+                        <Form.Item>
+                            <Button className='submit-button' type="primary" htmlType="submit">
+                                Lưu thay đổi
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </div>
-            </div >
+            </div>
             <Footer />
         </>
     );
