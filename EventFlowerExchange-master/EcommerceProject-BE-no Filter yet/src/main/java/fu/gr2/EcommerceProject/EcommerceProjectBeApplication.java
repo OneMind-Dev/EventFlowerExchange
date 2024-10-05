92a524e8d71de779569deaf29b38e6e12e104b8f
Arcane <0152ooms@gmail.com>
2024-10-05 22:38:41 +0700

    a

--- EventFlowerExchange-master/EcommerceProject-BE-no Filter yet/src/main/java/fu/gr2/EcommerceProject/EcommerceProjectBeApplication.java
+++ EventFlowerExchange-master/EcommerceProject-BE-no Filter yet/src/main/java/fu/gr2/EcommerceProject/EcommerceProjectBeApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceProjectBeApplication {
-	public EcommerceProjectBeApplication() {
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceProjectBeApplication.class, args);
 	}
+
 }
