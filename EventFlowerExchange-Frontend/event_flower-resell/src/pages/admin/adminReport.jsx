9e17f5d05b3cdff02ca0debe5cb9bccf655b2a61
PhuThang <thangtpse151058@fpt.edu.vn>
2024-10-07 10:33:53 +0700

    ReportPage

---
+++ EventFlowerExchange-Frontend/event_flower-resell/src/pages/admin/adminReport.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/header/header'
+import Footer from '../../components/footer/footer'
+
+function adminReport() {
+    return (
+        <div>
+            <Header />
+            Report List
+            <Footer />
+        </div>
+    )
+}
+
+export default adminReport
