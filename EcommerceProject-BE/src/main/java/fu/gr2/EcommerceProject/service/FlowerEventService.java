56c685dad7bb95f6d53c96bf8759882e6bf175fd
Hoang <hoangdnse182987@fpt.edu.vn>
2024-10-16 19:58:23 +0700

    bo sung service

--- EcommerceProject-BE/src/main/java/fu/gr2/EcommerceProject/service/FlowerEventService.java
+++ EcommerceProject-BE/src/main/java/fu/gr2/EcommerceProject/service/FlowerEventService.java
@@ -1,14 +1,18 @@
 package fu.gr2.EcommerceProject.service;
 
 import fu.gr2.EcommerceProject.entity.FlowerEventRelationship;
+import fu.gr2.EcommerceProject.repository.FlowerEventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
-public class FlowerEventRelationshipService {
+public class FlowerEventService {
+    @Autowired
+    private FlowerEventRepository flowerEventRepository;
     public List<FlowerEventRelationship> getByEventId(UUID eventId) {
-
+        return flowerEventRepository.findByEvent_EventId(eventId);
     }
 }
