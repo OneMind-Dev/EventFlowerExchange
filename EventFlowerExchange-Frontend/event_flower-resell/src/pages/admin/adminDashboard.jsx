fa4877d8b3af648910ccd40774ab425823c39a77
PhuThang <thangtpse151058@fpt.edu.vn>
2024-10-25 22:55:32 +0700

    fix dashboard css and add seller into dashboard

--- EventFlowerExchange-Frontend/event_flower-resell/src/pages/admin/adminDashboard.jsx
+++ EventFlowerExchange-Frontend/event_flower-resell/src/pages/admin/adminDashboard.jsx
@@ -1,4 +1,3 @@
-// src/pages/admin/AdminDashboard.jsx
 import React from "react";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
@@ -16,8 +15,11 @@ const AdminDashboard = () => {
         <h1 className="title">Bảng thống kê</h1>
         <div className="dashboard-content">
           <div className="left-content">
-            <button onClick={() => navigate("/manager")} className="admin-button">
+            <button onClick={() => navigate("/manager")} className="user-button">
               User List
+            </button>
+            <button onClick={() => navigate("/approve")} className="seller-button">
+              Seller List
             </button>
           </div>
           <div className="right-content">
