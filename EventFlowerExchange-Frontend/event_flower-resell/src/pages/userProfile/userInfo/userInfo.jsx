7ea0a349cf581ca7cc4c1b0290f630295111ecfb
PhuThang <thangtpse151058@fpt.edu.vn>
2024-10-25 16:11:34 +0700

    Fix bug 401 when update user

--- EventFlowerExchange-Frontend/event_flower-resell/src/pages/userProfile/userInfo/userInfo.jsx
+++ EventFlowerExchange-Frontend/event_flower-resell/src/pages/userProfile/userInfo/userInfo.jsx
@@ -1,31 +1,22 @@
 import { useState } from "react";
-import Header from "../../../components/header/header";
-import Footer from "../../../components/footer/footer";
-import "./userInfo.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Form, Input, Button, Popconfirm } from "antd";
 import { FaCircleUser } from "react-icons/fa6";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../../../redux/features/userSlice";
-import { useNavigate } from "react-router-dom";
-import api from "../../../components/config/axios";
-import { login } from "../../../redux/features/userSlice";
 import { toast } from "react-toastify";
+import api from "../../../components/config/axios";
+import Header from "../../../components/header/header";
+import Footer from "../../../components/footer/footer";
+import { logout, login } from "../../../redux/features/userSlice";
+import "./userInfo.css";
 
 function UserInfo() {
   const [avatarUrl, setAvatarUrl] = useState(null);
   const [editingField, setEditingField] = useState(null);
   const [form] = Form.useForm();
-
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
-
   const navigate = useNavigate();
-
-  console.log(user.address);
-
-  // const handleLogout = () => {
-  //   dispatch(logoutUser());
-  // };
 
   const handleEdit = (field) => {
     setEditingField(field);
@@ -35,10 +26,7 @@ function UserInfo() {
     try {
       const values = await form.validateFields([field]);
       const updatedField = { [field]: values[field] };
-
       const response = await api.put(`/users/${userId}`, updatedField);
-
-      console.log(response.data);
       dispatch(login(response.data));
       setEditingField(null);
       toast.success("Cập nhật thông tin thành công");
@@ -56,8 +44,19 @@ function UserInfo() {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
-      reader.onloadend = () => {
-        setAvatarUrl(reader.result); // Update avatarUrl with the new image
+      reader.onloadend = async () => {
+        const base64Image = reader.result;
+        setAvatarUrl(base64Image);
+        try {
+          const response = await api.put(`/users/${user.userId}`, {
+            avatar: base64Image,
+          });
+          dispatch(login(response.data));
+          toast.success("Ảnh đại diện đã được cập nhật thành công!");
+        } catch (error) {
+          console.error("Error updating avatar:", error);
+          toast.error("Cập nhật ảnh đại diện thất bại");
+        }
       };
       reader.readAsDataURL(file);
     }
@@ -75,11 +74,7 @@ function UserInfo() {
           <div className="user_container">
             <div className="user_infor">
               {avatarUrl ? (
-                <img
-                  src={avatarUrl}
-                  alt="User Avatar"
-                  className="user_avatar"
-                />
+                <img src={avatarUrl} alt="User Avatar" className="user_avatar" />
               ) : (
                 <FaCircleUser className="user_icon" />
               )}
@@ -105,11 +100,7 @@ function UserInfo() {
               <p>Thay đổi mật khẩu</p>
               <p>Đơn hàng</p>
               {user.role && user.role.includes("SELLER") && (
-                <p
-                  onClick={() => {
-                    navigate("/profile/sellermanage");
-                  }}
-                >
+                <p onClick={() => navigate("/profile/sellermanage")}>
                   Quản lý sự kiện
                 </p>
               )}
@@ -123,45 +114,14 @@ function UserInfo() {
               </Popconfirm>
             </div>
           </div>
+
           <div className="infor_container">
             <h1>Thông Tin Cá Nhân</h1>
             <Form layout="vertical" form={form}>
               {/* Username */}
               <div className="form-item">
                 <label htmlFor="username">Tên tài khoản</label>
-                {editingField === "username" ? (
-                  <Form.Item
-                    name="username"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Vui lòng nhập tên tài khoản!",
-                      },
-                    ]}
-                  >
-                    <Input placeholder="Tên tài khoản" />
-                  </Form.Item>
-                ) : (
-                  <p>{form.getFieldValue("username") || user.username}</p>
-                )}
-                {editingField === "username" ? (
-                  <>
-                    <Button
-                      type="primary"
-                      onClick={() => handleSave(user.userId, "username")}
-                    >
-                      Lưu
-                    </Button>
-                    <Button onClick={handleCancel}>Hủy</Button>
-                  </>
-                ) : (
-                  <Button
-                    className="edit-button"
-                    onClick={() => handleEdit("username")}
-                  >
-                    Thay đổi
-                  </Button>
-                )}
+                <p>{user.username}</p>
               </div>
 
               {/* Phone */}
@@ -170,101 +130,76 @@ function UserInfo() {
                 {editingField === "phone" ? (
                   <Form.Item
                     name="phone"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Vui lòng nhập số điện thoại!",
-                      },
-                    ]}
+                    rules={[{ required: true, message: "Vui lòng nhập số điện thoại!" }]}
                   >
-                    <Input placeholder="Số điện thoại" />
+                    <Input placeholder="Số điện thoại" defaultValue={user.phone} />
                   </Form.Item>
                 ) : (
-                  <p>{form.getFieldValue("phone") || user.phone}</p>
+                  <p>{user.phone}</p>
                 )}
                 {editingField === "phone" ? (
                   <>
-                    <Button
-                      type="primary"
-                      onClick={() => handleSave(user.userId, "phone")}
-                    >
+                    <Button type="primary" onClick={() => handleSave(user.userId, "phone")}>
                       Lưu
                     </Button>
                     <Button onClick={handleCancel}>Hủy</Button>
                   </>
                 ) : (
-                  <Button
-                    className="edit-button"
-                    onClick={() => handleEdit("phone")}
-                  >
+                  <Button className="edit-button" onClick={() => handleEdit("phone")}>
                     Thay đổi
                   </Button>
                 )}
               </div>
+
               {/* Email */}
               <div className="form-item">
                 <label htmlFor="email">Email</label>
                 {editingField === "email" ? (
                   <Form.Item
                     name="email"
-                    rules={[
-                      { required: true, message: "Vui lòng nhập email!" },
-                    ]}
+                    rules={[{ required: true, message: "Vui lòng nhập email!" }]}
                   >
-                    <Input placeholder="Email" />
+                    <Input placeholder="Email" defaultValue={user.email} />
                   </Form.Item>
                 ) : (
-                  <p>{form.getFieldValue("email") || user.email}</p>
+                  <p>{user.email}</p>
                 )}
                 {editingField === "email" ? (
                   <>
-                    <Button
-                      type="primary"
-                      onClick={() => handleSave(user.userId, "email")}
-                    >
+                    <Button type="primary" onClick={() => handleSave(user.userId, "email")}>
                       Lưu
                     </Button>
                     <Button onClick={handleCancel}>Hủy</Button>
                   </>
                 ) : (
-                  <Button
-                    className="edit-button"
-                    onClick={() => handleEdit("email")}
-                  >
+                  <Button className="edit-button" onClick={() => handleEdit("email")}>
                     Thay đổi
                   </Button>
                 )}
               </div>
+
               {/* Address */}
               <div className="form-item">
                 <label htmlFor="address">Địa chỉ</label>
                 {editingField === "address" ? (
                   <Form.Item
                     name="address"
-                    rules={[
-                      { required: true, message: "Vui lòng nhập địa chỉ!" },
-                    ]}
+                    rules={[{ required: true, message: "Vui lòng nhập địa chỉ!" }]}
                   >
-                    <Input className="address" placeholder="Địa chỉ" />
+                    <Input placeholder="Địa chỉ" defaultValue={user.address} />
                   </Form.Item>
                 ) : (
-                  <p>{form.getFieldValue("address") || user.address}</p>
+                  <p>{user.address}</p>
                 )}
                 {editingField === "address" ? (
                   <>
-                    <Button
-                      type="primary"
-                      onClick={() => handleSave(user.userId, "address")}
-                    >
+                    <Button type="primary" onClick={() => handleSave(user.userId, "address")}>
                       Lưu
                     </Button>
                     <Button onClick={handleCancel}>Hủy</Button>
                   </>
                 ) : (
-                  <Button
-                    className="edit-button"
-                    onClick={() => handleEdit("address")}
-                  >
+                  <Button className="edit-button" onClick={() => handleEdit("address")}>
                     Thay đổi
                   </Button>
                 )}
@@ -273,7 +208,6 @@ function UserInfo() {
           </div>
         </div>
       </div>
-
       <Footer />
     </>
   );
